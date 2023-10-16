f7b15fdeba02940e472d16ef39d1db237478032b
wb-fc932227 <wb-fc932227@alibaba-inc.com>
2023-10-16 14:45:30 +0800

    fix: yaml.load 解析失败，try catch 兼容错误

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,11 @@
-import * as core from '@serverless-devs/core';
-import ProgressBar from 'progress';
-import path from 'path';
-import crypto from 'crypto';
-import _ from 'lodash';
-import Table from 'tty-table';
-import yaml from 'js-yaml';
-import { includes } from 'lodash';
+import * as core from "@serverless-devs/core";
+import ProgressBar from "progress";
+import path from "path";
+import crypto from "crypto";
+import _ from "lodash";
+import Table from "tty-table";
+import yaml from "js-yaml";
+import { includes } from "lodash";
 
 const { fse, colors, inquirer } = core;
 const { green, white } = colors;
@@ -15,7 +15,7 @@ export function checkEdition(str: string) {
     return;
   }
   const { Edition } = yaml.load(str) as Record<string, any>;
-  if (Edition && Edition !== '2.0.0' && Edition !== '1.0.0') {
+  if (Edition && Edition !== "2.0.0" && Edition !== "1.0.0") {
     const message: string = `Edition must be 2.0.0 / 1.0.0 version. Please join DingTalk group: 33947367 for consultation`;
     throw new Error(message);
   }
@@ -25,41 +25,56 @@ export function getDefinitionPaths(str: string) {
   if (!str) {
     return {};
   }
-  const { services } = yaml.load(str) as Record<string, any>;
-  const definitionPaths: any = {}
-  _.forEach(services, (value: any, key: string) => {
-    const component: string = _.get(value, 'component', '');
-    const definition: string = _.get(value, 'props.definition', '');
-    const fnfComponents = ['devsapp/fnf', 'fnf', 'devsapp/fnf@dev', 'fnf@dev']
-    if (includes(fnfComponents, component) && definition) {
-      definitionPaths[key] = definition
-    }
-  })
-  return definitionPaths;
+  try {
+    const { services } = yaml.load(str) as Record<string, any>;
+    const definitionPaths: any = {};
+    _.forEach(services, (value: any, key: string) => {
+      const component: string = _.get(value, "component", "");
+      const definition: string = _.get(value, "props.definition", "");
+      const fnfComponents = [
+        "devsapp/fnf",
+        "fnf",
+        "devsapp/fnf@dev",
+        "fnf@dev",
+      ];
+      if (includes(fnfComponents, component) && definition) {
+        definitionPaths[key] = definition;
+      }
+    });
+    return definitionPaths;
+  } catch (err) {
+    return {};
+  }
 }
 
 export function createProgressBar(format, options) {
-  const opts = Object.assign({
-    complete: green('█'),
-    incomplete: white('█'),
-    width: 20,
-    clear: true,
-  }, options);
+  const opts = Object.assign(
+    {
+      complete: green("█"),
+      incomplete: white("█"),
+      width: 20,
+      clear: true,
+    },
+    options
+  );
   const bar = new ProgressBar(format, opts);
   const old = bar.tick;
-  const loadingChars = ['⣴', '⣆', '⢻', '⢪', '⢫'];
+  const loadingChars = ["⣴", "⣆", "⢻", "⢪", "⢫"];
   // @ts-ignore
   bar.tick = (len, tokens) => {
-    const newTokens = Object.assign({
-      loading: loadingChars[Math.random() * 5],
-    }, tokens);
+    const newTokens = Object.assign(
+      {
+        loading: loadingChars[Math.random() * 5],
+      },
+      tokens
+    );
     old.call(bar, len, newTokens);
   };
   return bar;
 }
 
 export function hasHttpPrefix(s: string): boolean {
-  return s.startsWith('http://');
+  return s.startsWith("http://");
 }
 
 export function capitalizeFirstLetter(s: string) {
@@ -74,16 +89,23 @@ export function extract(regex, endpoint, idx) {
   return null;
 }
 
-export function generateResourceName(serviceName: string, region: string, accountID: string) {
+export function generateResourceName(
+  serviceName: string,
+  region: string,
+  accountID: string
+) {
   const prefix = serviceName.slice(0, 6);
 
-  const md5Uid = crypto.createHmac('md5', accountID).update(serviceName).digest('hex');
+  const md5Uid = crypto
+    .createHmac("md5", accountID)
+    .update(serviceName)
+    .digest("hex");
   return `${prefix}-${md5Uid.slice(0, 7)}-${region}`;
 }
 
 export function formatArgs(args: string): string | null {
   // 去除 args 的行首以及行尾的空格
-  return (args ? args.replace(/(^\s*)|(\s*$)/g, '') : '');
+  return args ? args.replace(/(^\s*)|(\s*$)/g, "") : "";
 }
 
 /**
@@ -91,12 +113,18 @@ export function formatArgs(args: string): string | null {
  * @param serviceName 服务名称
  * @param functionName 函数名称
  */
-export async function checkBuildAvailable(serviceName: string, functionName: string, baseDir = process.cwd()) {
+export async function checkBuildAvailable(
+  serviceName: string,
+  functionName: string,
+  baseDir = process.cwd()
+) {
   const statusId = `${serviceName}-${functionName}-build`;
-  const statusPath = path.join(baseDir, '.s', 'fc-build');
-  const { status } = await core.getState(statusId, statusPath) || {};
-  if (status === 'unavailable') {
-    throw new Error(`${serviceName}/${functionName} build status is unavailable.Please re-execute 's build'`);
+  const statusPath = path.join(baseDir, ".s", "fc-build");
+  const { status } = (await core.getState(statusId, statusPath)) || {};
+  if (status === "unavailable") {
+    throw new Error(
+      `${serviceName}/${functionName} build status is unavailable.Please re-execute 's build'`
+    );
   }
 }
 
@@ -109,62 +137,63 @@ export async function checkBuildAvailable(serviceName: string, functionName: str
 export function getStateFilePath(id: any, dirPath?: string): string {
   const { templateFile } = process.env;
   const spath = fse.existsSync(templateFile)
-    ? path.join(path.dirname(templateFile), '.s')
-    : path.join(process.cwd(), '.s');
+    ? path.join(path.dirname(templateFile), ".s")
+    : path.join(process.cwd(), ".s");
   fse.ensureDirSync(spath);
   const temp = dirPath ? path.resolve(spath, dirPath) : spath;
   return path.join(temp, `${id}.json`);
 }
 
-
 export const tableShow = (data, showKey) => {
   const options = {
-    borderStyle: 'solid',
-    borderColor: 'blue',
-    headerAlign: 'center',
-    align: 'left',
-    color: 'cyan',
-    width: '100%',
+    borderStyle: "solid",
+    borderColor: "blue",
+    headerAlign: "center",
+    align: "left",
+    color: "cyan",
+    width: "100%",
   };
   const header_option = {
-    headerColor: 'cyan',
-    color: 'cyan',
-    align: 'left',
-    width: 'auto',
+    headerColor: "cyan",
+    color: "cyan",
+    align: "left",
+    width: "auto",
     formatter: (value) => value,
   };
 
-  const header = showKey.map((value) => (!_.isString() ? ({
-    ...header_option,
-    value,
-  }) : ({ ...header_option, ...value })));
+  const header = showKey.map((value) =>
+    !_.isString()
+      ? {
+          ...header_option,
+          value,
+        }
+      : { ...header_option, ...value }
+  );
 
   // eslint-disable-next-line no-console
   console.log(Table(header, data, options).render());
 };
 
-export async function promptForConfirmOrDetails(message: string): Promise<boolean> {
-  const answers: any = await inquirer.prompt([{
-    type: 'list',
-    name: 'prompt',
-    message,
-    choices: ['yes', 'no'],
-  }]);
+export async function promptForConfirmOrDetails(
+  message: string
+): Promise<boolean> {
+  const answers: any = await inquirer.prompt([
+    {
+      type: "list",
+      name: "prompt",
+      message,
+      choices: ["yes", "no"],
+    },
+  ]);
 
-  return answers.prompt === 'yes';
+  return answers.prompt === "yes";
 }
 
-export const sleep = (ms = 1000) => new Promise((resolve) => setTimeout(resolve, ms));
+export const sleep = (ms = 1000) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
 
 export function transfromTriggerConfig(triggerConfig, region, accountId) {
-  const {
-    name,
-    type,
-    config,
-    qualifier,
-    role,
-    sourceArn,
-  } = triggerConfig;
+  const { name, type, config, qualifier, role, sourceArn } = triggerConfig;
   if (_.isString(sourceArn) && !_.isNil(sourceArn)) {
     return {
       triggerName: name,
@@ -177,15 +206,17 @@ export function transfromTriggerConfig(triggerConfig, region, accountId) {
   }
   let arn;
 
-  if (type === 'oss') {
+  if (type === "oss") {
     arn = `acs:oss:${region}:${accountId}:${config.bucketName}`;
-  } else if (type === 'log') {
+  } else if (type === "log") {
     arn = `acs:log:${region}:${accountId}:project/${config.logConfig.project}`;
-  } else if (type === 'mns_topic') {
-    arn = `acs:mns:${config.region ? config.region : region}:${accountId}:/topics/${config.topicName}`;
-  } else if (type === 'cdn_events') {
+  } else if (type === "mns_topic") {
+    arn = `acs:mns:${
+      config.region ? config.region : region
+    }:${accountId}:/topics/${config.topicName}`;
+  } else if (type === "cdn_events") {
     arn = `acs:cdn:*:${accountId}`;
-  } else if (type === 'tablestore') {
+  } else if (type === "tablestore") {
     arn = `acs:ots:${region}:${accountId}:instance/${config.instanceName}/table/${config.tableName}`;
   }
 
@@ -199,10 +230,15 @@ export function transfromTriggerConfig(triggerConfig, region, accountId) {
   };
 }
 
-export function getTargetTriggers(sourceTriggers: any[], onlyDelpoyTriggerName: string | string[]) {
+export function getTargetTriggers(
+  sourceTriggers: any[],
+  onlyDelpoyTriggerName: string | string[]
+) {
   let needDeployTriggers = [];
   if (_.isString(onlyDelpoyTriggerName)) {
-    needDeployTriggers = sourceTriggers.filter(({ name }) => name === onlyDelpoyTriggerName);
+    needDeployTriggers = sourceTriggers.filter(
+      ({ name }) => name === onlyDelpoyTriggerName
+    );
     if (_.isEmpty(needDeployTriggers)) {
       throw new Error(`Not found trigger: ${onlyDelpoyTriggerName}`);
     }
@@ -230,7 +266,7 @@ export function getTargetTriggers(sourceTriggers: any[], onlyDelpoyTriggerName: 
 export function objectDeepTransfromString(source) {
   if (_.isArray(source)) {
     return source.map((value) => {
-      if (typeof value === 'object') {
+      if (typeof value === "object") {
         return objectDeepTransfromString(value);
       }
       return value?.toString();
@@ -239,7 +275,7 @@ export function objectDeepTransfromString(source) {
 
   if (_.isObject(source)) {
     return _.mapValues(source, (value) => {
-      if (typeof value === 'object') {
+      if (typeof value === "object") {
         return objectDeepTransfromString(value);
       }
       // @ts-ignore 不是 object 类型尝试 toString 强制转换为字符串
